eb287a9c46d893188f3ad07abef3a55d2b03640e
Lee Hamilton <lhamil64@gmail.com>
2014-10-22 21:04:32 -0400

    Added code to allow the user to create new nodes by touching the screen.

--- src/edu/clarkson/cs350/graph_designer/GraphNodeEntity.java
+++ src/edu/clarkson/cs350/graph_designer/GraphNodeEntity.java
@@ -9,6 +9,7 @@ import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.text.style.SuperscriptSpan;
 import android.util.Log;
 
 
@@ -20,8 +21,8 @@ import android.util.Log;
 public class GraphNodeEntity extends MultiTouchEntity {
 	private ArrayList<GraphNodeEntity> connectedNodes = new ArrayList<GraphNodeEntity>();
 
-	private final int RADIUS = 50; //px
-	private Paint paint;
+	private static final int RADIUS = 50; //px
+	private static Paint paint;
 	
 	public GraphNodeEntity() {
 		super();
@@ -36,11 +37,22 @@ public class GraphNodeEntity extends MultiTouchEntity {
 
 	@Override
 	public void draw(Canvas canvas) {
-		Log.d("cs350-graph", "GraphNodeEntity draw(canvas) called");
+		//Log.d("cs350-graph", "GraphNodeEntity draw(canvas) called");
 		
 		canvas.drawCircle(this.getCenterX(),
 				  this.getCenterY(),
 				  RADIUS, paint);
+	}
+	
+	public void setX(float x){
+		this.mCenterX = x;
+	}
+	public void setY(float y){
+		this.mCenterY = y;
+	}
+	public void setXY(float x, float y){
+		this.mCenterX = x;
+		this.mCenterY = y;
 	}
 
 	@Override
@@ -55,4 +67,22 @@ public class GraphNodeEntity extends MultiTouchEntity {
 
 	}
 
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		int base = super.hashCode();
+		base += mCenterX*mCenterY;
+		base += 13;
+		return base;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("(%f, %f)", mCenterX, mCenterY);
+	}
 }
--- src/edu/clarkson/cs350/graph_designer/GraphView.java
+++ src/edu/clarkson/cs350/graph_designer/GraphView.java
@@ -1,6 +1,7 @@
 package edu.clarkson.cs350.graph_designer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import android.content.Context;
 import android.content.res.Configuration;
@@ -21,94 +22,113 @@ import org.metalev.multitouch.controller.MultiTouchController.PointInfo;
 import org.metalev.multitouch.controller.MultiTouchController.PositionAndScale;
 import org.metalev.multitouch.controller.MultiTouchEntity;
 
-public class GraphView extends View
-						implements MultiTouchObjectCanvas<MultiTouchEntity>{
+public class GraphView extends View implements
+		MultiTouchObjectCanvas<MultiTouchEntity> {
 
 	private static final int UI_MODE_ROTATE = 1, UI_MODE_ANISOTROPIC_SCALE = 2;
 	private int mUIMode = UI_MODE_ROTATE;
-	
-	private MultiTouchController<MultiTouchEntity> multiTouchController = 
-	        new MultiTouchController<MultiTouchEntity>(this);
-	
+
+	private MultiTouchController<MultiTouchEntity> multiTouchController = new MultiTouchController<MultiTouchEntity>(
+			this);
+
 	private ArrayList<GraphNodeEntity> nodes = new ArrayList<GraphNodeEntity>();
-	
+
 	private PointInfo currTouchPoint = new PointInfo();
 	private Paint mLinePaintTouchPointCircle = new Paint();
+
+	private static final float SCREEN_MARGIN = 100;
+
+	private GraphNodeEntity currentDrag = new GraphNodeEntity(0,0,mLinePaintTouchPointCircle);
 	
-    private static final float SCREEN_MARGIN = 100;
-    
-    private static float RADIUS = 50;
-    
-    private int width, height, displayWidth, displayHeight;
-    
-    //-------
-	
+	private int width, height, displayWidth, displayHeight;
+
+	// -------
+
 	public GraphView(Context context) {
 		this(context, null);
 	}
+
 	public GraphView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
+
 	public GraphView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		init(context);
 	}
-	
-	private void init(Context context){
+
+	private void init(Context context) {
 		Resources res = context.getResources();
 		
+		// Set up the colors and line width of the circles and lines
 		mLinePaintTouchPointCircle.setColor(Color.YELLOW);
 		mLinePaintTouchPointCircle.setStrokeWidth(5);
 		mLinePaintTouchPointCircle.setStyle(Style.STROKE);
 		mLinePaintTouchPointCircle.setAntiAlias(true);
 		setBackgroundColor(Color.BLACK);
-		
+
 		DisplayMetrics metrics = res.getDisplayMetrics();
-        this.displayWidth = res.getConfiguration().orientation == 
-            Configuration.ORIENTATION_LANDSCAPE ? Math.max(metrics.widthPixels,
-            metrics.heightPixels) : Math.min(metrics.widthPixels, metrics.heightPixels);
-        this.displayHeight = res.getConfiguration().orientation == 
-            Configuration.ORIENTATION_LANDSCAPE ? Math.min(metrics.widthPixels,
-            metrics.heightPixels) : Math.max(metrics.widthPixels, metrics.heightPixels);
-        
-            nodes.add(new GraphNodeEntity(100, 100, mLinePaintTouchPointCircle));
+		this.displayWidth = res.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE ? Math
+				.max(metrics.widthPixels, metrics.heightPixels) : Math.min(
+				metrics.widthPixels, metrics.heightPixels);
+		this.displayHeight = res.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE ? Math
+				.min(metrics.widthPixels, metrics.heightPixels) : Math.max(
+				metrics.widthPixels, metrics.heightPixels);
+		
+		// Manually adding a node to test
+		nodes.add(new GraphNodeEntity(100, 100, mLinePaintTouchPointCircle));
 	}
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
-		Log.d("cs350-graph", "GraphView onDraw(canvas) called");
+		/* This method is called any time the screen needs to be re-drawn
+		 * (when invalidate() is called).
+		 */
+		Log.d("cs350-graph", "onDraw called");
+		Log.d("cs350-cur", "nodes: " + nodes.toString());
 		
-		for (GraphNodeEntity x : nodes){
+		super.onDraw(canvas);
+		canvas.drawColor(Color.BLACK);
+		
+		// Draw every node in the "nodes" ArrayList
+		for (GraphNodeEntity x : nodes) {
 			x.draw(canvas);
 		}
 		
+		// Set xs and ys to arrays of points covered by the user's finger
+		// Set numPoints to the number of fingers touching the screen
 		float[] xs = currTouchPoint.getXs();
 		float[] ys = currTouchPoint.getYs();
-		float[] pressures = currTouchPoint.getPressures();
 		int numPoints = Math.min(currTouchPoint.getNumTouchPoints(), 2);
-		
-		if (numPoints == 2){
+
+		if (numPoints == 2) {
 			// draw line connecting the two nodes
-		} else if (numPoints == 1){
+		} else if (numPoints == 1) {
 			// draw a new node
-			GraphNodeEntity tmp = new GraphNodeEntity(xs[0], ys[0], mLinePaintTouchPointCircle); 
-			nodes.add(tmp);
-			tmp.draw(canvas);
+			Log.d("cs350-graph", "Creating node at " + xs[0] + "," + ys[0]);
+			currentDrag.setXY(xs[0], ys[0]);
+			currentDrag.draw(canvas);
 		}
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		Log.d("cs350-graph", "GraphView onTouchEvent called");
-		invalidate();
+		if (event.getAction() == MotionEvent.ACTION_UP){
+			// If the touch action is lifting their finger
+			nodes.add(currentDrag);
+			currentDrag = new GraphNodeEntity(0,0,mLinePaintTouchPointCircle);
+		}
 		return multiTouchController.onTouchEvent(event);
 	}
 
 	@Override
 	public MultiTouchEntity getDraggableObjectAtPoint(PointInfo touchPoint) {
+		currTouchPoint.set(touchPoint);
+		invalidate();
+		
 		float x = touchPoint.getX(), y = touchPoint.getY();
-		for (GraphNodeEntity gne : nodes){
-			if (gne.containsPoint(x, y)){
+		for (GraphNodeEntity gne : nodes) {
+			if (gne.containsPoint(x, y)) {
 				return gne;
 			}
 		}
@@ -118,24 +138,30 @@ public class GraphView extends View
 	@Override
 	public boolean pointInObjectGrabArea(PointInfo touchPoint,
 			MultiTouchEntity obj) {
-		// TODO Auto-generated method stub
+		Log.d("cs350-graph", "GraphView pointInObjectGrabArea called");
 		return false;
 	}
 
 	@Override
 	public void getPositionAndScale(MultiTouchEntity obj,
 			PositionAndScale objPosAndScaleOut) {
+		Log.d("cs350-graph", "GraphView getPositionAndScale called");
 		
-		objPosAndScaleOut.set(obj.getCenterX(), obj.getCenterY(), (mUIMode & UI_MODE_ANISOTROPIC_SCALE) == 0,
-				(obj.getScaleX() + obj.getScaleY()) / 2, (mUIMode & UI_MODE_ANISOTROPIC_SCALE) != 0, obj.getScaleX(), obj.getScaleY(),
-				(mUIMode & UI_MODE_ROTATE) != 0, obj.getAngle());
+		objPosAndScaleOut.set(obj.getCenterX(), obj.getCenterY(),
+				(mUIMode & UI_MODE_ANISOTROPIC_SCALE) == 0,
+				(obj.getScaleX() + obj.getScaleY()) / 2,
+				(mUIMode & UI_MODE_ANISOTROPIC_SCALE) != 0, obj.getScaleX(),
+				obj.getScaleY(), (mUIMode & UI_MODE_ROTATE) != 0,
+				obj.getAngle());
 	}
 
 	@Override
 	public boolean setPositionAndScale(MultiTouchEntity obj,
 			PositionAndScale newObjPosAndScale, PointInfo touchPoint) {
+		Log.d("cs350-graph", "GraphView setPositionAndScale called");
+		
 		currTouchPoint.set(touchPoint);
-		boolean ok = ((GraphNodeEntity)obj).setPos(newObjPosAndScale);
+		boolean ok = ((GraphNodeEntity) obj).setPos(newObjPosAndScale);
 		if (ok)
 			invalidate();
 		return ok;
@@ -143,8 +169,9 @@ public class GraphView extends View
 
 	@Override
 	public void selectObject(MultiTouchEntity obj, PointInfo touchPoint) {
+		Log.d("cs350-graph", "GraphView selectObject called");
 		currTouchPoint.set(touchPoint);
-		if (obj != null){
+		if (obj != null) {
 			// Received a valid node object,
 			// push it to the top of the stack
 			nodes.remove(obj);
@@ -154,5 +181,5 @@ public class GraphView extends View
 		}
 		invalidate();
 	}
-	
+
 }
